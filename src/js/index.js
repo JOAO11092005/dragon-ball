3929cf07ab3abdfa3a964cc6a6562ef7b7dbf33f
João Paulo Neves Batista <joaopaulonevesbatista@gmail.com>
2025-04-05 22:03:22 -0300

    atualizar

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,28 @@
-const botoes = document.querySelectorAll(".botao");
-const personagens = document.querySelectorAll(".personagem");
+//passo 1 & 2 
+const botoes = document.querySelectorAll('.botao');
+const personagem = document.querySelectorAll('.personagem');
 
-botoes.forEach((botao, indice) => {
-	botao.addEventListener("click", () => {
-		desselecionarBotao();
-		desselecionarPersonagem();
+botoes.forEach(function(botao , indece){
+	botao.addEventListener('click', function(){
+		//passo 3 importante
+		deseleciona()
+        const botaoSelecionado = document.querySelector('.botao.selecionado');
+		
+		botaoSelecionado.classList.remove('selecionado')
+		console.log(botaoSelecionado);
 
-		botao.classList.add("selecionado");
-		personagens[indice].classList.add("selecionado");
-	});
-});
+		botao.classList.add('selecionado');
+		let selecao = personagem[indece].classList.add('selecionado')
+		
+		
+	})
+})
 
-function desselecionarPersonagem() {
-	const personagemSelecionado = document.querySelector(".personagem.selecionado");
-	personagemSelecionado.classList.remove("selecionado");
-}
 
-function desselecionarBotao() {
-	const botaoSelecionado = document.querySelector(".botao.selecionado");
-	botaoSelecionado.classList.remove("selecionado");
+const deseleciona = function(){
+	const personagemSelecionado = document.querySelectorAll(".personagem.selecionado");
+
+	personagemSelecionado.forEach(function(selecionado){
+		selecionado.classList.remove('selecionado')
+	})
 }
